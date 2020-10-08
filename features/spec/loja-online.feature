7620ef2dd1dd61f7a9d05fadc96c2d9af283afc2
rclarindo <rodrigo.clarindo@fcamara.com.br>
2020-10-08 11:36:37 -0300

    ajustando os testes

--- features/spec/loja-online.feature
+++ features/spec/loja-online.feature
@@ -5,18 +5,16 @@ Funcionalidade: Portal de Vendas Online Magazine Luiza
     Eu como cliente das lojas Magazine Luiza gostaria de uma loja Online
     Para realizar minhas buscas e compras por produtos
 
-    @produto_sacola
+    @pesquisar_produto
     Cenario: Realizar busca de  um produto e inseri-lo na sacola
         Dado que eu esteja no site da Magazine Luiza pela "url"
-        E no campo de pesquisa eu informo o nome de um produto "TV"
-        Quando o resultado do produto "TV" aparecer na tela clico no produto
-        E clico no botão Adicionar à sacola
-        Então produto "TV" aparece na sacola de compras
+        Quando pesquiso o nome do produto "Smart TV LED 40"
+        Então o produto "Smart TV LED 40" é apresentado com sucesso
 
     @produto_inexistente
     Cenario: Realizar busca de  um produto e inseri-lo no carrinho
         Dado que eu esteja no site da Magazine Luiza pela "url"
-        Quando no campo de pesquisa eu informo o nome de um produto "79898uyiyuy"
+        Quando pesquiso o nome do produto "79898uyiyuy"
         Então mensagem é informada "não encontrou resultado algum"
 
 
--- features/step_definitions/loja-online.rb
+++ features/step_definitions/loja-online.rb
@@ -4,25 +4,15 @@ Dado('que eu esteja no site da Magazine Luiza pela {string}') do |site|
   @magazine_luiza.url(site)
 end
 
-Dado('no campo de pesquisa eu informo o nome de um produto {string}') do |produto|
+Quando("pesquiso o nome do produto {string}") do |produto|
   @magazine_luiza.realizar_busca(produto)
 end
 
-Quando('o resultado do produto {string} aparecer na tela clico no produto') do |produto|
-  expect(find('#main-title > strong').text).to have_content produto
-  achei_produto = find('#product_193425500 > div.nm-product-info > h2')
-  achei_produto.click  
-end
-
-Quando('clico no botão Adicionar à sacola') do
-  click_button 'Adicionar à sacola'
-end
-
-Então('produto {string} aparece na sacola de compras') do |produto|
-  expect(find('.header-tip__product-txt').text).to have_content produto
+Então("o produto {string} é apresentado com sucesso") do |produto|
+  expect(page).to have_content produto
 end
 
 # ===================== Produto inexistente===========================
 Então('mensagem é informada {string}') do |mensagem|
-  expect(find('.nm-not-found-message1').text).to have_content mensagem
+  expect(page).to have_content mensagem
 end
